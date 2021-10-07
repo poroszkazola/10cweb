5ea90dd3a743811a45ec50efa33b2710e240d04f
Poroszka Zoltán <poroszka.zoltan@sulinet.csmszki>
2021-10-07 09:36:15 +0200

    elagazas

--- 21.09.30/muveletek.py
+++ 21.09.30/muveletek.py
@@ -4,3 +4,51 @@ print(round(3.1415, 3))
 # hatvanyozas
 print(pow(2, 8)) #pow(alap, kitevo)
 #abszolutertek abs(mit)
+print(abs(-12))
+print(abs(12))
+print(min(12,7,9,45,3))
+print(max(12,7,9,45,3))
+# pi
+import math
+a=math.pi
+print(a)
+''
+from math import * # a program elejen helyezzuk el
+a=pi
+print(a)
+#negyzetgyok -sqrt
+print(sqrt(16))    #math.sqrt(mibol vonok gyokot)
+# felfele kerekites - math.ceil(mit kerekitek)
+print(ceil(3.16))
+#lefele kerekites -math.floor(mit)
+print(floor(3.56))
+
+
+# 1. negyzet kerulete terulete
+a=float(input("kerem a negyzet oldalat! a="))
+k=4*a
+t=pow(a,2)
+print("a(z)",a, "egyseg oldalu negyzet kerulete", k, "terulete",t)
+
+print("A negyzet kerulete",k)
+print("a negyzet terulete",t)
+
+
+# 2. teglalap kerulete,terulete
+print(" a teglalap kerulete",k)
+print("a teglalap terulete",t)
+
+
+
+# 3. a kor kerulete (2*r/PI), terulete (r^2*PI)
+
+
+
+
+# 4. a kocka terfogata (V=a^3), felszine (A=6*a^2)
+
+
+
+
+# 5. derekszogu haromszog 2 befogojat kerd be, add meg az atfogot!
+# c=negyzetgyok(a^2+bˇ2)
